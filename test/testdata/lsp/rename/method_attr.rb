85ff52df1b72a4eb1db17c97c9d1de5e3b804373
Nathan Froyd <froydnj@gmail.com>
2022-04-27 06:31:13 -0900

    add some more method rename tests

--- test/testdata/lsp/rename/method_attr.rb
+++ test/testdata/lsp/rename/method_attr.rb
@@ -14,3 +14,10 @@ end
 f = Foo.new
 f.foo
 #  ^ apply-rename: [B] newName: bar invalid: true expectedErrorMessage: Sorbet does not support renaming `attr_reader`s
+f.bar = 5
+# ^ apply-rename: [F] newName: bar2 invalid: true
+f.baz
+#   ^ apply-rename: [G] newName: baz2 invalid: true
+f.baz = 9
+#   ^ apply-rename: [H] newName: baz2 invalid: true
+
